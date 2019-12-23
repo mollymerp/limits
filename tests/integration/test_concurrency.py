3f3687a95f8caba061928ca441ba5a8eae8551bd
Ali-Akber Saifee <ali@indydevs.org>
2019-12-23 11:24:40 -0800

    Switch to py.test

--- tests/integration/test_concurrency.py
+++ tests/integration/test_concurrency.py
@@ -1,16 +1,19 @@
 import sys
 import time
 import threading
+import unittest
+
+import pytest
+
 from tests.integration import IntegrationTest
-from parameterized import parameterized
 from uuid import uuid4
 from limits.storage import MemoryStorage
 from limits.strategies import FixedWindowRateLimiter, MovingWindowRateLimiter
 from limits.limits import RateLimitItemPerSecond
 
-class ConcurrencyTests(IntegrationTest):
-    def setUp(self):
-        super(ConcurrencyTests, self).setUp()
+
+@pytest.mark.integration
+class ConcurrencyTests(unittest.TestCase):
 
     def test_memory_storage_fixed_window(self):
         storage = MemoryStorage()
